1d0cbdf86b7188b9132123da6f55c4f1a23b4497
rureu <rureu@thefinders.kr>
2023-08-10 12:53:50 +0900

    카카오페이연동 샘플

--- vkazelv.page/Portfolio/Controllers/AccountController.cs
+++ vkazelv.page/Portfolio/Controllers/AccountController.cs
@@ -3,7 +3,15 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication.Google; 
-using RureuLib.OAuth.KakaoTalk; 
+using RureuLib.OAuth.KakaoTalk;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Net.Http.Json;
+using System.Text;
+using System.Web;
+using System.Net;
+using System.Text.Json;
+using Portfolio.Models.KakaoPay;
 
 namespace Portfolio.Controllers
 {
@@ -81,6 +89,61 @@ namespace Portfolio.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+        public async Task<IActionResult> KakaoPayAsync()
+        {
+
+            string url = "https://kapi.kakao.com/v1/payment/ready";
+            string authorization = "f4a2ec54ca4e48690137cb6e1a0f3219";
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("KakaoAK", authorization);
+
+
+            Dictionary<string, string> reqParams = new Dictionary<string, string>();
+            reqParams.Add("cid", "TC0ONETIME"); // 가맹점 코드, 10자
+            //reqParams.Add("cid_secret", "");
+            reqParams.Add("partner_order_id", "testorder1");
+            reqParams.Add("partner_user_id", "test1");
+            reqParams.Add("item_name", "test");
+            //reqParams.Add("item_code", "");
+            reqParams.Add("quantity", "1");
+            reqParams.Add("total_amount", "100");
+            reqParams.Add("tax_free_amount", "10");
+            //reqParams.Add("vat_amount", "");
+            //reqParams.Add("green_deposit", "");
+            reqParams.Add("approval_url", "https://localhost:7075/");
+            reqParams.Add("cancel_url", "https://localhost:7075/");
+            reqParams.Add("fail_url", "https://localhost:7075/");
+            //reqParams.Add("available_cards", "");
+            //reqParams.Add("payment_method_type", "");
+            //reqParams.Add("install_month", "");
+            //reqParams.Add("custom_json", "");
+
+
+
+          //  StringContent scData = new(System.Text.Json.JsonSerializer.Serialize(reqParams), Encoding.UTF8, "application/x-www-form-urlencoded");
+
+            using HttpResponseMessage response = await httpClient.PostAsync(url, new FormUrlEncodedContent(reqParams));
+
+
+            if (response.IsSuccessStatusCode)
+            { 
+                string resContent = await response.Content.ReadAsStringAsync();
+                if (resContent != null)
+                {
+                    /// json string.
+                    string tag  = HttpUtility.UrlDecode(resContent, Encoding.UTF8);
+                    ReadyRsp obj = JsonSerializer.Deserialize<ReadyRsp>(tag);
+
+                    return Redirect(obj.next_redirect_pc_url);
+                }
+            }
+
+            return View();
+        }
         #endregion
     }
 }
---
+++ vkazelv.page/Portfolio/Models/KakaoPay/ReadyRsp.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Models.KakaoPay
+{
+    public class ReadyRsp
+    {
+        public string tid { get; set; }
+        public string next_redirect_app_url { get; set; }
+        public string next_redirect_mobile_url { get; set; }
+        public string next_redirect_pc_url { get; set; }
+        public string android_app_scheme { get; set; }
+        public string ios_app_scheme { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
